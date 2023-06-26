2f3ffd6bbf5f7eb9a9c858336c89373ec9c65493
Prabhjot Singh <prabhjot@webmobsoft.com>
2023-06-26 13:39:20 +0530

    first commit

--- src/app/chat/chat-routing.module.ts
+++ src/app/chat/chat-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
 
 const routes: Routes = [{
   path: '',
-  loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule)
+  component: ChatComponent
 }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class ChatRoutingModule { }
